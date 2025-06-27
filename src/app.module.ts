2cc87f726a3180dd9f5c4f07ee9f40ed480d4a93
alexdev <alexdev.workenv@gmail.com>
2025-06-27 01:07:08 -0400

    configure graphql

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { PostsModule } from './posts/posts.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PostsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/posts/posts.resolver.ts
@@ -0,0 +1,14 @@
+import { Resolver } from '@nestjs/graphql';
+import { PostsService } from './posts.service';
+import { Query } from '@nestjs/graphql';
+import { Post } from './post.entity';
+
+@Resolver()
+export class PostsResolver {
+  constructor(private postsService: PostsService) {}
+
+  @Query(() => [Post])
+  posts() {
+    return this.postsService.findAll();
+  }
+}
