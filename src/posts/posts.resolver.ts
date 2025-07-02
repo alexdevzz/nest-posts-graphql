16f121f084d8b47e63e329aa67caaea7168f6864
alexdev <alexdev.workenv@gmail.com>
2025-07-01 20:56:56 -0400

    add mutations method for create posts

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,14 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
-import { PostsService } from './posts.service';
-import { Query } from '@nestjs/graphql';
-import { Post } from './post.entity';
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { PostsService } from './posts.service'
+import { Query } from '@nestjs/graphql'
+import { Post } from './post.entity'
+import { CreatePostInput } from './dto/create-post.input'
 
 @Resolver()
 export class PostsResolver {
-  constructor(private postsService: PostsService) {}
+  constructor(private postsService: PostsService) { }
 
   @Query(() => [Post])
   posts() {
-    return this.postsService.findAll();
+    return this.postsService.findAll()
+  }
+
+  @Mutation(() => Post)
+  createPost(@Args('postInput') postInput: CreatePostInput) {
+    return this.postsService.createPost(postInput)
   }
 }
